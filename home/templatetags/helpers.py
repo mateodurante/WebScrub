1aec2b5ae7a01ef3accfd2f07e41cb88cd715f47
Mateo Durante <mateodurante@gmail.com>
2022-02-15 19:09:28 -0300

    cambios vistas

--- home/templatetags/helpers.py
+++ home/templatetags/helpers.py
@@ -35,7 +35,7 @@ def convert_size(size_bytes):
         return size_bytes
     if size_bytes == 0:
         return "0 B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    size_name = ("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
     i = int(math.floor(math.log(size_bytes, 1024)))
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
